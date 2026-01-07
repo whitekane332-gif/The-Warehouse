65b5d63e01b2923ef418d7bdd4a495315c368d76
whitekane332-gif <whitekane332@gmail.com>
2026-01-07 18:06:59 +0800

    Update image-edit.js

--- functions/api/image-edit.js
+++ functions/api/image-edit.js
@@ -1,14 +1,28 @@
-export async function onRequestPost({ env }) {
+export async function onRequestPost({ request, env }) {
+  const { prompt } = await request.json();
+
+  const res = await fetch("https://api.openai.com/v1/images/generations", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-image-1",
+      prompt: prompt,
+      size: "512x512",
+    }),
+  });
+
+  const data = await res.json();
+
   return new Response(
     JSON.stringify({
       ok: true,
-      message: "Function is alive",
-      hasKey: !!env.OPENAI_API_KEY,
+      image: data.data[0].url,
     }),
     {
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
     }
   );
 }
